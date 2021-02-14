ed82dda0b4528fb803222e8b01f69b1b556bcfae
Lena Hou <lenahou2013@gmail.com>
2021-02-14 20:07:41 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -14,12 +14,7 @@ def parse
 # The [] bracket notation says to match any characer in the setter
 # The + tells to treat multiple spaces between emails as a single separator
 parsed_emails = @email_addresses.split(/[,\s]+/)
-parsed_emails.each do |email|
-if email == email
-  parsed_emails.delete(email)
- end
-end
-parsed_emails
+
 end
 
 end
